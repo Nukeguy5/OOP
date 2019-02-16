49e26ee9fef68708981f7532c72c03a245b111e4
devonrudd <devon.rudd@jessup.edu>
2019-02-16 12:18:52 -0800

    Added for_loop_is_sorted

--- Homework/generatorlistsort.py
+++ Homework/generatorlistsort.py
@@ -18,8 +18,22 @@ def next_is_greater(gen, curr):
         return True
     except TypeError:
         return False
+        
+def for_loop_is_sorted(*lst):
+    if type(lst[0]) == list:
+        gen = (i for i in lst[0])
+    else:
+        gen = (i for i in lst)
+    curr = next(gen)
+    for i in gen:
+        if curr >= i:
+            return False
+        curr = i
+    return True
 
 my_list = [i for i in range(10)]
 my_list2 = [1, 2, 10, 3, 4]
 print(is_sorted(my_list))
 print(is_sorted(my_list2))
+print(for_loop_is_sorted(my_list))
+print(for_loop_is_sorted(my_list2))
