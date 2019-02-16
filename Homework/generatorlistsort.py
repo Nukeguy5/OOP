30fc818ac7cb17e788771993c90c4c06e70a4bae
devonrudd <devon.rudd@jessup.edu>
2019-02-16 14:45:16 -0800

    Implemented into 'List' class

--- Homework/generatorlistsort.py
+++ Homework/generatorlistsort.py
@@ -19,20 +19,20 @@
 #         return True
 #     except TypeError:
 #         return False
-        
-def is_sorted(*lst):
-    if type(lst[0]) == list:
-        gen = (i for i in lst[0])
-    else:
-        gen = (i for i in lst)
-    curr = next(gen)
-    for i in gen:
-        if curr >= i:
-            return False
-        curr = i
-    return True
 
-my_list = [i for i in range(10)]
-my_list2 = [1, 2, 10, 3, 4]
-print(is_sorted(my_list))
-print(is_sorted(my_list2))
+class List:
+    def __init__(self, *args):
+        self.gen = (i for i in args)
+       
+    def is_sorted(self):
+        curr = next(self.gen)
+        for i in self.gen:
+            if curr >= i:
+                return False
+            curr = i
+        return True
+
+my_list = List(1, 2, 3, 4, 5)
+my_list2 = List(1, 2, 10, 4, 5)
+print(my_list.is_sorted())
+print(my_list2.is_sorted())
