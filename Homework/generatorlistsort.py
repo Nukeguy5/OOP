276a3769ec481ce769f8d5e4ad646718083c1e12
devonrudd <devon.rudd@jessup.edu>
2019-02-16 12:08:34 -0800

    Added generatorlistsort.py

--- Homework/generatorlistsort.py
+++ Homework/generatorlistsort.py
@@ -1,2 +1,25 @@
 
+def is_sorted(*lst):
+    if type(lst[0]) == list:
+        gen = (i for i in lst[0])
+    else:
+        gen = (i for i in lst)
+    first = next(gen)
+    return next_is_greater(gen, first)
 
+def next_is_greater(gen, curr):
+    nxt = next(gen)
+    try:
+        if curr <= nxt:
+            return next_is_greater(gen, nxt)
+        else:
+            return False
+    except StopIteration:
+        return True
+    except TypeError:
+        return False
+
+my_list = [i for i in range(10)]
+my_list2 = [1, 2, 10, 3, 4]
+print(is_sorted(my_list))
+print(is_sorted(my_list2))
