c0bd1a07539670e20ae2b1e76ddbce47cd1b0e38
devonrudd <devon.rudd@jessup.edu>
2019-02-16 12:20:19 -0800

    Submission Ready

--- Homework/generatorlistsort.py
+++ Homework/generatorlistsort.py
@@ -1,23 +1,23 @@
 
-def is_sorted(*lst):
-    if type(lst[0]) == list:
-        gen = (i for i in lst[0])
-    else:
-        gen = (i for i in lst)
-    first = next(gen)
-    return next_is_greater(gen, first)
+# def is_sorted(*lst):
+#     if type(lst[0]) == list:
+#         gen = (i for i in lst[0])
+#     else:
+#         gen = (i for i in lst)
+#     first = next(gen)
+#     return next_is_greater(gen, first)
 
-def next_is_greater(gen, curr):
-    nxt = next(gen)
-    try:
-        if curr <= nxt:
-            return next_is_greater(gen, nxt)
-        else:
-            return False
-    except StopIteration:
-        return True
-    except TypeError:
-        return False
+# def next_is_greater(gen, curr):
+#     nxt = next(gen)
+#     try:
+#         if curr <= nxt:
+#             return next_is_greater(gen, nxt)
+#         else:
+#             return False
+#     except StopIteration:
+#         return True
+#     except TypeError:
+#         return False
         
 def for_loop_is_sorted(*lst):
     if type(lst[0]) == list:
@@ -33,7 +33,7 @@ def for_loop_is_sorted(*lst):
 
 my_list = [i for i in range(10)]
 my_list2 = [1, 2, 10, 3, 4]
-print(is_sorted(my_list))
-print(is_sorted(my_list2))
+# print(is_sorted(my_list))
+# print(is_sorted(my_list2))
 print(for_loop_is_sorted(my_list))
 print(for_loop_is_sorted(my_list2))
