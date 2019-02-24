98f39dbcc4a283e5e9d819c2e37220bae185cf9a
Nukeguy5 <nukeguy5@gmail.com>
2019-02-24 09:15:17 -0800

    Updated enumerated input

--- Homework/enumeratedinput.py
+++ Homework/enumeratedinput.py
@@ -1,2 +1,18 @@
 
 from enum import Enum
+from collections import namedtuple
+
+names = 'BEES BIRDS BALLOONS'
+
+# UserIn = Enum('UserIn', input('Enter Input: '))
+UserEnum = Enum('UserIn', names)
+
+Tup = namedtuple('Tup', [i.name for i in UserEnum])
+
+UserFind = input('Enter Name to Find: ')
+
+for i in UserEnum:
+    if i.name = UserFind:
+        
+
+
