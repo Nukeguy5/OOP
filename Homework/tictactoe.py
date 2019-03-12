93a472795c7fcee6940bd7bfdece7dd1ec3798aa
Nukeguy5 <nukeguy5@gmail.com>
2019-03-12 11:28:58 -0700

    Added tictactoe.py

---
+++ Homework/tictactoe.py
@@ -0,0 +1,23 @@
+
+import random
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [[None, None, None] for _ in range(3)]
+
+    def place_letter(self, letter, location):
+        return
+
+
+def LogTime(func):
+    def wrapper(*args, **kwargs):
+        return
+    return wrapper
+
+def PrintBoard():
+    def wrapper():
+        return
+    return wrapper
+
+game = TicTacToe()
+print(game.board)
