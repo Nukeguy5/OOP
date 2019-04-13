dbc9469c066704d924e6302af7c1646f74b7bd11
Nukeguy5 <nukeguy5@gmail.com>
2019-04-13 14:45:47 -0700

    Added main() function Clears dictionary when program exits

--- Homework/flyweightpattern.py
+++ Homework/flyweightpattern.py
@@ -6,27 +6,36 @@
 # If an input of a string is already in the dictionary, don’t add it but give an error saying it already exists and show what tag it has. 
 # If the tag is the same then change the string value of that tag. At the end, print out the string list.
 
-# If string is same, don't add. (key)
-# If tag is same, add/update. (value)
+# If string is same, don't add. (value)
+# If tag is same, add/update. (tag)
 
 class Flywieght:
     _StringDict = {}
  
     @classmethod
-    def add_to_dict(cls):
-        tag = input("Enter a tag for the string: ")
-        string = input("Enter a string: ")
-        if string in cls._StringDict:
-            print(f"Error: String already exists with tag '{cls._StringDict[string]}'.")
-        elif tag == cls._StringDict[string]:
-            # Ask about this
-
-            return
+    def update_dict(cls, tag, string):
+        for k, v in cls._StringDict.items():
+            if string == v:
+                print(f"ERROR: String '{string} already exists with tag '{k}'.")
+                return
+        cls._StringDict[tag] = string
 
     @classmethod
-    def show_string_dict(cls):
-        for string in cls._StringDict:
-            key = string
-            val = cls._StringDict[key]
-            print(f"{key}: {val}")
+    def print_strings(cls):
+        print('-'*6, "Final", '-'*6)
+        for key, val in cls._StringDict.items():
+            print(f"{key} => {val}")
+        print('-'*19)
 
+def main():
+    while True:
+        tag = input("Enter a tag (or /q to quit): ")
+        if tag == "/q":
+            break
+        string = input("Enter a string: ")
+        Flywieght.update_dict(tag, string)
+    Flywieght.print_strings()
+    Flywieght._StringDict.clear()
+
+if __name__ == "__main__":
+    main()
